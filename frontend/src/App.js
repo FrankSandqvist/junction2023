ec7d311cf7ea9211df086286dbfa5f39a84aeb64
Frank Sandqvist <kankki@gmail.com>
2023-11-10 22:21:51 +0200

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="absolute w-full h-full flex items-center justify-center">
+      <header className=" w-64 h-64 bg-fuchsia-800 text-fuchsia-100 font-bold rounded-lg shadow-lg shadow-fuchsia-400 flex items-center justify-center">
+        Woot!
+      </header>
+    </div>
+  );
+}
+
+export default App;
